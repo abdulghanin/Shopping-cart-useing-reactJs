5f1912495e841226ee62db6d49c6ae24df230a8e
abdulghani <abdulghani7827@gmail.com>
2023-04-08 04:16:19 -0700

    shopping cart useing react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data  from "./data.json";
+import Products from "./components/Products";
+import Filter from "./components/Filter";
+import Cart from "./components/Cart";
 
-function App() {
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      cartItems:localStorage.getItem("cartItems")? JSON.parse(localStorage.getItem("cartItems")):[],
+      size:"",
+      sort:"",
+    
+    };
+  }
+  createOrder= (order)=>{
+    alert("New to save order" + order.name)
+  }
+  removeFromCart =(product)=>{
+    const cartItems= this.state.cartItems.slice( );
+    this.setState({
+      cartItems: cartItems.filter((x)=> x._id !== product._id)
+    })
+    localStorage.setItem("cartItems",JSON.stringify(cartItems.filter((x) =>x._id === product._id)));
+  }
+  addToCart =(product) => {
+    const cartItems= this.state.cartItems.slice( );
+    let alreadyInCart = false;
+    cartItems.forEach((item)=> {
+      if (item._id === product._id){
+        item.count ++;
+        alreadyInCart= true;
+      }
+    });
+    if(!alreadyInCart){
+      cartItems.push({...product, count: 1});
+    }
+    this.setState({cartItems});
+    localStorage.setItem("cartItems",JSON.stringify(cartItems));
+  }
+  sortProducts = (event) =>{
+    const sort= event.target.value;
+    console.log(event.target.value);
+    this.setState((state)=>({
+      sort:sort,
+      products: this.state.products.slice().sort((a,b)=>
+        sort ==="lowest"?
+        a.price > b.price ? 1:-1:
+        sort ==="highest"?
+        a.price < b.price? 1:-1:
+        a._id > b._id ?1:-1
+      )
+    }))
+  }
+  filterProducts =(event) =>{
+    console.log(event.target.value);
+    if(event.target.value === ""){
+      this.setState({size: event.target.value, products: data.products});
+    }else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        )
+      })
+    }
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+     <div className="grid-container">
+            <header>
+              <a href="/">Shopping Cart</a>
+            </header>
+            <main className="content">
+              <div className="main">
+                <Filter conut={this.state.products.length}
+                  size={this.state.size}
+                  sort={this.state.sort}
+                  filterProducts={this.filterProducts}
+                  sortProducts={this.sortProducts}>
+                </Filter>
+              <Products products={this.state.products} addToCart={this.addToCart}></Products>
+              </div>
+              <div className="sidebar">
+                 <Cart cartItems={this.state.cartItems}  
+                 removeFromCart={this.removeFromCart}
+                 createOrder={this.createOrder}></Cart> 
+              </div>
+            </main>
+            <footer>All right is reserved.</footer>
     </div>
   );
+}
 }
 
 export default App;
